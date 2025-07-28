4c3f26dc1f4d208d0fc4875bd599ca572c25ae28
Atanu Debnath <atanuc2005@gmail.com>
2025-07-28 19:55:31 +0530

    Added features like updating item details, sorting items, calculating total worth, adding new item later.

--- product.cpp
+++ product.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class shop
-{
+class shop{
     int counter;
     string itemname[50];
     int itemid[50];
@@ -15,7 +14,12 @@ public:
     void getprice();
     void searchid(int id);
     void display();
+    void update(int id);
+    void sort();
+    void total();
+    void additem();
 };
+
 void shop::getname(){
     cout << "Enter the name of the item number " << counter + 1 << ": ";
     cin >> itemname[counter];
@@ -30,8 +34,9 @@ void shop::getprice(){
     counter++;
 }
 void shop::display(){
-    cout << "\n========= Shop Inventory =========\n";
-    for (int i = 0; i < counter; i++){
+    cout << "\\n========= Shop Inventory =========\\n";
+    for (int i = 0; i < counter; i++)
+    {
         cout << "Item #" << i + 1 << endl;
         cout << "Name  : " << itemname[i] << endl;
         cout << "ID    : " << itemid[i] << endl;
@@ -41,8 +46,10 @@ void shop::display(){
 }
 void shop::searchid(int id){
     bool found = false;
-    for (int i = 0; i < counter; i++){
-        if (itemid[i] == id){
+    for (int i = 0; i < counter; i++)
+    {
+        if (itemid[i] == id)
+        {
             cout << "Item found with ID " << id << " !" << endl;
             cout << "Item name: " << itemname[i] << endl;
             cout << "Item price: " << itemprice[i] << " Rupees" << endl;
@@ -50,10 +57,110 @@ void shop::searchid(int id){
             break;
         }
     }
-    if (!found){
+    if (!found)
+    {
         cout << "Item with ID " << id << " not  found" << endl;
     }
 }
+void shop::update(int id){
+    bool found = false;
+    for (int i = 0; i < counter; i++){
+        if (itemid[i] == id){
+            found = true;
+            cout << "What do you want to update? [(/name), (/price)]" << endl;
+            string up;
+            cin >> up;
+            if (up == "/name"){
+                cout << "Enter the new name: ";
+                cin >> itemname[i];
+                cout << "Process succesful!!" << endl;
+            }
+            else if (up == "/price"){
+                cout << "Enter the new price: ";
+                cin >> itemprice[i];
+                cout << "Process succesful!!" << endl;
+            }
+            else{
+                cout << "Invalid command detected" << endl;
+            }
+        }
+    }
+    if (!found){
+        cout << "No item found with certain ID" << endl;
+    }
+}
+void shop::sort(){
+    int sortedprice[50];
+    cout << "Enter 'a' for ascending, 'b' for descending: ";
+    char sh;
+    cin >> sh;
+    for (int i = 0; i < counter; i++){
+        sortedprice[i] = itemprice[i];
+    }
+
+    if (sh == 'b'){
+        for (int i = 0; i < counter - 1; i++){
+            for (int j = i + 1; j < counter; j++){
+                if (sortedprice[i] < sortedprice[j]){
+                    swap(sortedprice[i], sortedprice[j]);
+                }
+            }
+        }
+        cout << "\\n========= Sorted Inventory (Descending by Price) =========\\n";
+            for (int i = 0; i < counter; i++){
+                for (int j = 0; j < counter; j++){
+                    if (itemprice[j] == sortedprice[i]){
+                    cout << "Item #" << i + 1 << endl;
+                    cout << "Name  : " << itemname[j] << endl;
+                    cout << "ID    : " << itemid[j] << endl;
+                    cout << "Price : " << itemprice[j] << " Rupees" << endl;
+                    cout << "------------------------------" << endl;
+                    break;
+                }
+            }
+        }
+    } else if (sh == 'a'){
+        for (int i = 0; i < counter - 1; i++){
+            for (int j = i + 1; j < counter; j++){
+                if (sortedprice[i] > sortedprice[j]){
+                    int temp = sortedprice[i];
+                    sortedprice[i] = sortedprice[j];
+                    sortedprice[j] = temp;
+                }
+            }
+        }
+        cout << "\\n========= Sorted Inventory (Ascending by Price) =========\\n";
+        for (int i = 0; i < counter; i++) {
+            for (int j = 0; j < counter; j++) {
+                if (itemprice[j] == sortedprice[i]) {
+                    cout << "Item #" << i + 1 << endl;
+                    cout << "Name  : " << itemname[j] << endl;
+                    cout << "ID    : " << itemid[j] << endl;
+                    cout << "Price : " << itemprice[j] << " Rupees" << endl;
+                    cout << "------------------------------" << endl;
+                    break;
+                }
+            }
+        }
+    } else {
+        cout << "Invalid command detected" << endl;
+    }
+}
+void shop::total(){
+    cout << "Total number of items: " << counter << endl;
+    int total = 0;
+    for(int i = 0; i < counter; i++){
+        total += itemprice[i];
+    }
+    cout << "Total worth: " << total << "  Rupees" << endl;
+}
+void shop::additem(){
+    getname();
+    getid();
+    getprice();
+    display();
+}
+
 int main(){
     shop product;
     int n;
@@ -66,20 +173,41 @@ int main(){
         product.getprice();
     }
     product.display();
-    char c;
+    cout << endl;
+    string cmd;
     int id;
-    cout << "Do you want to search an item? [y/n]" << endl;
-    cout << "=> ";
-    cin >> c;
-    while (c == 'y'){
-        cout << "Enter the item id: ";
-        cin >> id;
-        product.searchid(id);
-        cout << "Do you want to search an item again? [y/n]" << endl;
+    while (true) {
+        cout << "Choose command [(/update), (/sort), (/total), (/add), (/search), (/end)]" << endl;
         cout << "=> ";
-        cin >> c;
-    }
-    cout << "Thank you for using the shop management system!" << endl;
+        cin >> cmd;
 
+        if (cmd == "/update") {
+            cout << "Enter the id: ";
+            cin >> id;
+            product.update(id);
+        } else if (cmd == "/sort") {
+            product.sort();
+        } else if (cmd == "/total") {
+            product.total();
+        } else if (cmd == "/add") {
+            product.additem();
+        } else if (cmd == "/search") {
+            char c = 'y';
+            while (c == 'y') {
+                cout << "Enter the item id: ";
+                cin >> id;
+                product.searchid(id);
+                cout << "Do you want to search an item again? [y/n]" << endl;
+                cout << "=> ";
+                cin >> c;
+            }
+        } else if (cmd == "/end") {
+            break;
+        } else {
+            cout << "Invalid command!!" << endl;
+        }
+    }
+
+    cout << "Thank you for using the shop management system!" << endl;
     return 0;
 }
